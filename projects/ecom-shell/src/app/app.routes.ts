3597801ae548a1f4233ca462774c15db582f8d65
Nikesh Pansare <nikesh.pansare@gmail.com>
2025-12-19 22:15:38 +0530

    feat: added native federation configuration

--- projects/ecom-shell/src/app/app.routes.ts
+++ projects/ecom-shell/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => loadRemoteModule('home', './home').then((m) => m.routes),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
