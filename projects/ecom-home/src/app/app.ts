7b17ad5b8e2139aee77175c07275978fa9d771d8
Nikesh Pansare <nikesh.pansare@gmail.com>
2025-12-19 21:46:09 +0530

    feat: created basic structure for project

--- projects/ecom-home/src/app/app.ts
+++ projects/ecom-home/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('dashboard');
+  protected readonly title = signal('ecom-home');
 }
--- projects/ecom-shell/src/app/app.ts
+++ projects/ecom-shell/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('profile');
+  protected readonly title = signal('ecom-shell');
 }
