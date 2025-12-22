ee02aed8d04127dcdd16a56cbf92cddaa107dfa5
Nikesh Pansare <nikesh.pansare@gmail.com>
2025-12-22 13:41:00 +0530

    feat: can use lit components library in project

--- projects/ecom-home/src/app/home-component/home-component.ts
+++ projects/ecom-home/src/app/home-component/home-component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home-component',
   imports: [],
   templateUrl: './home-component.html',
   styleUrl: './home-component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
